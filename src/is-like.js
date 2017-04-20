6278b03ffeb7d12e43df5ae9d47a29f7fd8d841b
Tony L. Kerz <anthony.kerz@gmail.com>
2017-04-20 15:50:28 -0400

    [is-like] dbg not-is-like

--- src/is-like.js
+++ src/is-like.js
@@ -1,6 +1,11 @@
 import _ from 'lodash'
+import debug from 'debug'
+
+const dbg = debug('app:helpr:is-like')
 
 const assertRe = /^assert\((.+)\)$/
+
+/* eslint-disable no-unused-expressions */
 
 export const isLikeHooks = {
   assert: {
@@ -16,21 +21,29 @@ export const isLikeHooks = {
 
 export function isLike({expected, actual, hooks}) {
   if (_.isArray(expected)) {
-    return isArrayLike({expected, actual, hooks})
+    const result = isArrayLike({expected, actual, hooks})
+    result || _dbg({expected, actual})
+    return result
   }
 
   if (_.isObject(expected)) {
-    return isObjectLike({expected, actual, hooks})
+    const result = isObjectLike({expected, actual, hooks})
+    result || _dbg({expected, actual})
+    return result
   }
 
   if (hooks) {
     const hook = _.find(hooks, elt => elt.shouldTrigger({expected, actual}))
     if (hook) {
-      return hook.isLike({expected, actual})
+      const result = hook.isLike({expected, actual})
+      result || _dbg({expected, actual})
+      return result
     }
   }
 
-  return expected === actual
+  const result = (expected === actual)
+  // result || _dbg({expected, actual})
+  return result
 }
 
 function isArrayLike({expected, actual, hooks}) {
@@ -38,7 +51,9 @@ function isArrayLike({expected, actual, hooks}) {
     _.isArray(actual) &&
     (expected.length === actual.length) &&
     _.every(expected, (elt, index) => {
-      return isLike({expected: elt, actual: actual[index], hooks})
+      const result = isLike({expected: elt, actual: actual[index], hooks})
+      result || dbg('!is-like: index=%o, expected=%j, actual=%j', index, elt, actual[index])
+      return result
     })
   )
 }
@@ -47,7 +62,13 @@ function isObjectLike({expected, actual, hooks}) {
   return (
     _.isObject(actual) &&
     _.every(expected, (elt, key) => {
-      return isLike({expected: elt, actual: actual[key], hooks})
+      const result = isLike({expected: elt, actual: actual[key], hooks})
+      result || dbg('!is-like: key=%o, expected=%j, actual=%j', key, elt, actual[key])
+      return result
     })
   )
+}
+
+function _dbg({expected, actual}) {
+  dbg('!is-like: expected=%j, actual=%j', expected, actual)
 }
